6a3e4aced45bd4bb5eb00455738df42d0332c4ef
binhdnguyen <nguyenducbinh@gmail.com>
2025-08-21 16:51:18 +0700

    Update Google Sheets WebApp URL in configuration example

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 // Example configuration file
 // Copy this to config.js and replace with your actual values
 const CONFIG = {
-    GOOGLE_SHEETS_WEBAPP_URL: 'https://script.google.com/u/1/home/projects/1QfQHBPQj2nizMDzTbkZGzORZegX4xxuyT2u5Qt0kPN_-eZaX-kzYhrVg/edit'
+    GOOGLE_SHEETS_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycby-2r_Uvx2zL7qiqeWSrDdG2xupdiRVllTDwKBz6iRpmwu38sq46agJ04wqBNmisEXI/exec'
 };
